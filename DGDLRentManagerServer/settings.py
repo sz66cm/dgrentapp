bb78ee037353bc0ba5788563dd5a8ef068501fb2
ChenMing <sz66cm@163.com>
2016-08-12 20:44:15 +0800

    edit setting.py dbs config!

--- DGDLRentManagerServer/settings.py
+++ DGDLRentManagerServer/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'DGDLRentManagerServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DGDLManagerDB',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
